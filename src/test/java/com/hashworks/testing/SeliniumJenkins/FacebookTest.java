d2dbec4cd7b5b1444f589feb1ed507b353813f0e
pranay <pranay.saha@hashworks.co>
2018-02-23 12:26:50 +0530

    ip changes

--- src/test/java/com/hashworks/testing/SeliniumJenkins/FacebookTest.java
+++ src/test/java/com/hashworks/testing/SeliniumJenkins/FacebookTest.java
@@ -53,7 +53,7 @@ public class FacebookTest {
 	URL server;
 	String browser_name = "chrome";
 	try {
-		server = new URL("http://172.16.0.52:4444/wd/hub");
+		server = new URL("http://172.16.0.35:4444/wd/hub");
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 	    capabilities.setBrowserName(browser_name);
 
